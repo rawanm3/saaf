d8db6e3a2a1b5bca722ad43762763fc99b0676d5
bassant <bassanthossny400@gmail.com>
2025-09-02 00:31:07 +0300

    aading pop-up in list

--- Lahomes-Angular/src/app/views/customers/list/list.component.ts
+++ Lahomes-Angular/src/app/views/customers/list/list.component.ts
@@ -4,15 +4,28 @@ import { customerData } from '../data'
 import {
   NgbDropdownModule,
   NgbPaginationModule,
+  NgbModal, NgbModalModule,
 } from '@ng-bootstrap/ng-bootstrap'
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [PageTitleComponent, NgbPaginationModule, NgbDropdownModule],
+  imports: [CommonModule, PageTitleComponent, NgbPaginationModule, NgbDropdownModule, NgbModalModule],
   templateUrl: './list.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ListComponent {
   customerList = customerData
+// -----------------------------bassant-------------------------------------------------
+
+   selectedCustomer: any | null = null;
+
+  constructor(private modalService: NgbModal) {}
+
+  openCustomerModal(tpl: any, customer: any) {
+    this.selectedCustomer = customer;
+    this.modalService.open(tpl, { size: 'lg', centered: true });
+  }
 }
