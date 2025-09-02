ce5b8e454f97e781a53604c78e688d8ca38c8e6a
mohamed alaa <mohamed5336053@gmail.com>
2025-09-02 12:36:14 +0300

    add serarch inputs

--- Lahomes-Angular/src/app/views/customers/list/list.component.ts
+++ Lahomes-Angular/src/app/views/customers/list/list.component.ts
@@ -5,14 +5,43 @@ import {
   NgbDropdownModule,
   NgbPaginationModule,
 } from '@ng-bootstrap/ng-bootstrap'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [PageTitleComponent, NgbPaginationModule, NgbDropdownModule],
+  imports: [
+    PageTitleComponent,
+    NgbPaginationModule,
+    NgbDropdownModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './list.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ListComponent {
   customerList = customerData
+  filteredCustomers = [...this.customerList]
+
+  searchText: string = ''
+  private searchTimeout: any
+
+  filterCustomers() {
+    clearTimeout(this.searchTimeout)
+
+    this.searchTimeout = setTimeout(() => {
+      const search = this.searchText.toLowerCase().trim()
+      this.filteredCustomers = this.customerList.filter(
+        (c) =>
+          c.name.toLowerCase().includes(search) ||
+          c.email.toLowerCase().includes(search) ||
+          c.phone.toLowerCase().includes(search) ||
+          c.type.toLowerCase().includes(search) ||
+          c.address.toLowerCase().includes(search) ||
+          c.customerStatus.toLowerCase().includes(search)
+      )
+    }, 300)
+  }
 }
